06a95cf0d5fccceef3a087be0b0ad262305779ef
joel donkor <donkorjoel203@gmail.com>
2025-01-31 22:21:58 -0800

    Added a Search component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { ThemeProvider } from './components/ThemeContext';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
 
 function App() {
   return (
     <ThemeProvider>
       <div className="min-h-screen dark:bg-gray-900 dark:text-white">
         <Navbar />
+        <Search />
         {/* Add other components here */}
       </div>
     </ThemeProvider>
