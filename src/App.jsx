5def4c62c5391cf7924c9610b5f32866505a2bff
joel donkor <donkorjoel203@gmail.com>
2025-02-28 17:41:17 -0800

    Added JobContainer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 import React from 'react';
 import { ThemeProvider } from './components/ThemeContext';
 import Navbar from './components/Navbar';
-import JobSearch from './components/JobSearch';
+import JobContainer from './components/JobContainer';
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen dark:bg-gray-900 dark:text-white">
+      <div className="min-h-screen  bg-white dark:bg-gray-900 dark:text-white">
         <Navbar />
-        <JobSearch />
+        <JobContainer />
+        
         {/* Add other components here */}
       </div>
     </ThemeProvider>
