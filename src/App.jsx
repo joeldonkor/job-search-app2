2357f47b0266356d3651f328f1a62fbc488381c2
joel donkor <donkorjoel203@gmail.com>
2025-03-07 22:35:26 -0800

    Added react router for different pages and the imported the components of the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-// src/App.jsx
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import HomePage from './pages/HomePage';
 import { ThemeProvider } from './components/ThemeContext';
-import Navbar from './components/Navbar';
-import JobContainer from './components/JobContainer';
+ 
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen  bg-white dark:bg-gray-900 dark:text-white">
-        <Navbar />
-        <JobContainer />
-        
-        {/* Add other components here */}
+      <div className="min-h-screen  bg-[#F8F8FA] dark:bg-gray-900 dark:text-white">
+          <Router>
+            
+              
+            <Routes>
+              
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+              
+              
+            
+            </Routes>
+          
+        </Router>
       </div>
+      
     </ThemeProvider>
+     
+    
   );
 }
 
