1b86e940b2ac333571ebaebce6e0d13c49b935fd
joel donkor <donkorjoel203@gmail.com>
2025-03-07 22:51:57 -0800

    Home page for the job search app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+
+import Navbar from '../components/Navbar';
+import JobContainer from '../components/JobContainer';
+
+function HomePage() {
+  return (
+    <div>
+      
+        <Navbar />
+        <JobContainer />
+        
+        {/* Add other components here */}
+    </div>
+    
+  );
+}
+
+export default HomePage;
