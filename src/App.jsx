c72f92519865c7de86535270c14a16639cffc9e9
joel donkor <donkorjoel203@gmail.com>
2025-03-30 21:05:27 -0700

    Change routing from browse router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Changed here
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import HomePage from './pages/HomePage';
 import { ThemeProvider } from './components/ThemeContext';
- 
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen  bg-[#F8F8FA] dark:bg-gray-900 dark:text-white">
-          <Router>
-            
-              
-            <Routes>
-              
-                <Route path="/" element={<HomePage />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-              
-              
-            
-            </Routes>
-          
+      <div className="min-h-screen bg-[#F8F8FA] dark:bg-gray-900 dark:text-white">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
         </Router>
       </div>
-      
     </ThemeProvider>
-     
-    
   );
 }
 
